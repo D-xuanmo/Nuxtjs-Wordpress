a6a78d132320a08dc05c296e981f06c2e5aca5d7
xuanmos <me@xuanmo.xin>
2018-06-08 23:01:26 +0800

    init

---
+++ pages/page.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="">
+111
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
