1b5f1c41fcff21d0c1e2f92546a11f420c43304d
xuanmos <me@xuanmo.xin>
2018-12-09 16:01:24 +0800

    bug🤗g🐛

--- pages/details/_id.vue
+++ pages/details/_id.vue
@@ -232,7 +232,7 @@ export default {
   mounted () {
     // 海报内容
     this.posterContent = {
-      imgUrl: this.article.articleInfor.thumbnail.replace(/https?:\/\/([a-z\d-]\.?)+(\/.*)/gi, `${this.$store.state.info.baseUrl}$2`),
+      imgUrl: this.article.articleInfor.thumbnail.replace(/(https?:\/\/([a-z\d-]\.?)+)?(\/.*)/gi, `${this.$store.state.info.baseUrl}$3`),
       title: this.article.title.rendered,
       summary: this.article.articleInfor.summary,
       time: this.article.date.replace(/T.*/, ' '),
