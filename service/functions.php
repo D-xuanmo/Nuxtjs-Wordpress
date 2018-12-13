7a61166d689707eff5c85de6f0a4eb5db14c0f4d
xuanmos <me@xuanmo.xin>
2018-12-13 23:34:42 +0800

    样式修改

--- service/functions.php
+++ service/functions.php
@@ -133,8 +133,7 @@ function appthemes_add_quicktags()
         QTags.addButton('c-code', 'c-code', '<span class="code">', '</span>');
         QTags.addButton('h2', 'h2', '<h2>', '</h2>');
         QTags.addButton('2-text', '2-text', '<span style="display:inline-block;width:28px;">', '</span>');
-        QTags.addButton('star', 'star', '<i class="iconfont icon-star" class="c-theme">', '</i>');
-        QTags.addButton('red-star', 'red-star', '<i class="iconfont icon-star" style="color: #f00;">', '</i>');
+        QTags.addButton('star', 'star', '<i class="iconfont icon-star c-theme">', '</i>');
         // 添加html转换容器
         jQuery(function () {
             jQuery('#html-transform').click(function () {
