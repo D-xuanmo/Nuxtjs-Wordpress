03ffb8d32ae699af85a65f73d015424704fc37b7
xuanmos <me@xuanmo.xin>
2018-12-15 19:01:49 +0800

    update

---
+++ components/Icon/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <i class="iconfont" :class="type"></i>
+</template>
+<script>
+export default {
+  name: 'XIcon',
+  props: {
+    type: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style lang="scss" scoped></style>
