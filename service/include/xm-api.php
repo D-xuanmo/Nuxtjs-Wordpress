2839da2586b334b92ac9f570c002b4d15dcc45dc
xuanmos <me@xuanmo.xin>
2018-12-16 20:36:31 +0800

    修改会员等级规则

--- service/include/xm-api.php
+++ service/include/xm-api.php
@@ -300,21 +300,22 @@ function get_author_class ($comment_author_email)
   $author_count = count($wpdb -> get_results("SELECT comment_ID as author_count FROM $wpdb->comments WHERE comment_author_email = '$comment_author_email'"));
   if ($comment_author_email == $adminEmail) {
     return array('style' => $styleClass, 'level' => 'vip7', 'admin' => true, 'title' => '博主');
-  }
-  if ($author_count >= 1 && $author_count < 10 && $comment_author_email != $adminEmail) {
-    return array('style' => $styleClass, 'level' => 'vip1', 'title' => 'LV.1');
-  } else if ($author_count >= 10 && $author_count < 20 && $comment_author_email != $adminEmail) {
-    return array('style' => $styleClass, 'level' => 'vip2', 'title' => 'LV.2');
-  } else if ($author_count >= 20 && $author_count < 30 && $comment_author_email != $adminEmail) {
-    return array('style' => $styleClass, 'level' => 'vip3', 'title' => 'LV.3');
-  } else if ($author_count >= 30 && $author_count < 50 && $comment_author_email != $adminEmail) {
-    return array('style' => $styleClass, 'level' => 'vip4', 'title' => 'LV.4');
-  } else if ($author_count >= 50 && $author_count < 80 && $comment_author_email != $adminEmail) {
-    return array('style' => $styleClass, 'level' => 'vip5', 'title' => 'LV.5');
-  } else if ($author_count >= 80 && $author_count < 200 && $comment_author_email != $adminEmail) {
-    return array('style' => $styleClass, 'level' => 'vip6', 'title' => 'LV.6');
-  } else if ($author_count >= 200 && $comment_author_email != $adminEmail) {
-    return array('style' => $styleClass, 'level' => 'vip7', 'title' => '博主好基友');
+  } else {
+    if ($author_count >= 1 && $author_count < 10) {
+      return array('style' => $styleClass, 'level' => 'vip1', 'title' => 'LV.1');
+    } else if ($author_count >= 10 && $author_count < 20) {
+      return array('style' => $styleClass, 'level' => 'vip2', 'title' => 'LV.2');
+    } else if ($author_count >= 20 && $author_count < 40) {
+      return array('style' => $styleClass, 'level' => 'vip3', 'title' => 'LV.3');
+    } else if ($author_count >= 40 && $author_count < 80) {
+      return array('style' => $styleClass, 'level' => 'vip4', 'title' => 'LV.4');
+    } else if ($author_count >= 80 && $author_count < 160) {
+      return array('style' => $styleClass, 'level' => 'vip5', 'title' => 'LV.5');
+    } else if ($author_count >= 160 && $author_count < 300) {
+      return array('style' => $styleClass, 'level' => 'vip6', 'title' => 'LV.6');
+    } else if ($author_count >= 300) {
+      return array('style' => $styleClass, 'level' => 'vip7', 'title' => '博主好基友');
+    }
   }
 }
 
