81e85734cc6aea9f34c3d3c3e8f76b57a4f2c82d
xuanmos <me@xuanmo.xin>
2019-04-08 18:02:16 +0800

    Delete watch.js

--- watch.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const exec = require('child_process').exec
-
-fs.watch(`${__dirname}/service`, { recursive: true }, (eventType, fileName) => {
-  const time = new Date(+new Date() + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
-  console.log(time, fileName)
-  exec('cp -r service/* /Applications/MAMP/htdocs/wp-content/themes/xm-vue-theme', err => err && console.log(err))
-})
