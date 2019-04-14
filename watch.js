2e5ad85657f5c129a710ff0238bf7f72c056848a
xuanmos <me@xuanmo.xin>
2019-04-14 19:19:36 +0800

    update

--- watch.js
+++ watch.js
@@ -4,5 +4,5 @@ const exec = require('child_process').exec
 fs.watch(`${__dirname}/service`, { recursive: true }, (eventType, fileName) => {
   const time = new Date(+new Date() + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
   console.log(time, fileName)
-  exec('cp -r service/* /Applications/MAMP/htdocs/wp-content/themes/xm-vue-theme', err => err && console.log(err))
+  exec('cp -r service/* /Applications/MAMP/htdocs/wp-content/themes/xm-nuxt-theme', err => err && console.log(err))
 })
