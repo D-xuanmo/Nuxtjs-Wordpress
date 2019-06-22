4c78c675926c84bc7c9032c01a4cce2b25161bc5
xuanmos <me@xuanmo.xin>
2019-06-22 12:27:55 +0800

    修改axios拦截

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,17 +1,20 @@
 export default function ({ $axios, redirect }) {
   $axios.onRequest(config => {
     if (config.data) {
-      let data = {}
-      for (let [key, value] of Object.entries(config.data)) {
-        key !== 'progress' && (data[key] = value)
-      }
       if (config.method === 'get') {
         config.progress = config.data.progress
       } else {
+        let ContentType = config.headers['Content-Type']
+        if (ContentType && ContentType.indexOf('multipart/form-data') === -1) {
+          let data = {}
+          for (let [key, value] of Object.entries(config.data)) {
+            key !== 'progress' && (data[key] = value)
+          }
+          config.data = data
+        }
         config.progress = config.headers.progress
         delete config.headers.progress
       }
-      config.data = data
     }
     return config
   })
